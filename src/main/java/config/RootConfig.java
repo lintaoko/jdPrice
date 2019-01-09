e0c1529ff297ec15ea0bb04dacc07cc4a0b8a054
xiantang <986494553@qq.com>
2019-01-09 19:02:24 +0800

    添加评论

--- src/main/java/config/RootConfig.java
+++ src/main/java/config/RootConfig.java
@@ -1,9 +1,8 @@
 package config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 @Configuration
 @Import({CachingConfig.class
@@ -12,4 +11,12 @@ import org.springframework.context.annotation.ImportResource;
         , WebConfig.class})
 @ComponentScan(basePackages = "controller")
 public class RootConfig {
+
+    @Bean
+    public ViewResolver cnViewResolver(){
+        // 确定请求媒体类型
+
+        return new ContentNegotiatingViewResolver();
+    }
+
 }
--- src/main/java/controller/ProductIdController.java
+++ src/main/java/controller/ProductIdController.java
@@ -3,13 +3,21 @@ package controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import service.ProductIdService;
 
-@Controller
+@RestController
 @RequestMapping("/productId")
 public class ProductIdController {
     @Autowired
     ProductIdService productIdService;
+
+
+//    @RequestMapping(method = RequestMethod.GET)
+//    public
+
+
 
 
 
