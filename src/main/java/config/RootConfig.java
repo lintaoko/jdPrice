124c0ea13ecfa3645afe68051dd0b27007c978e3
xiantang <986494553@qq.com>
2019-01-09 15:37:40 +0800

    使用springMVC开启restful服务

--- src/main/java/config/RootConfig.java
+++ src/main/java/config/RootConfig.java
@@ -1,10 +1,14 @@
 package config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@Import({CachingConfig.class, MysqlConfig.class})
+@Import({CachingConfig.class
+        , MysqlConfig.class
+        , MethodSecurityConfig.class})
+@ComponentScan(basePackages = "controller")
 public class RootConfig {
 }
