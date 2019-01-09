06014ea245fb8fcead3c3dad0bb3b2e362952acc
xiantang <986494553@qq.com>
2019-01-09 15:18:22 +0800

    添加安全依赖

---
+++ src/main/java/config/MethodSecurityConfig.java
@@ -0,0 +1,13 @@
+package config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@Configuration
+@EnableGlobalMethodSecurity
+public class MethodSecurityConfig {
+
+
+
+}
