bc31e7b3efe38709cdd8bf31c842b432c3371d82
xiantang <986494553@qq.com>
2019-01-10 20:13:22 +0800

    添加Error实体类

--- src/main/java/controller/ProductIdController.java
+++ src/main/java/controller/ProductIdController.java
@@ -2,28 +2,66 @@ package controller;
 
 import model.ProductId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import service.ProductIdService;
 
 @RestController
 @RequestMapping("/productId")
-    public class ProductIdController {
+public class ProductIdController {
     @Autowired
     ProductIdService productIdService;
 
 
-    @RequestMapping(method = RequestMethod.GET,
-    produces = "application/json")
-    public @ResponseBody ProductId productId(
-            @RequestParam(value = "id") String id
-    ){
-        return productIdService.selectByPrimaryKey(id);
+//    @RequestMapping(method = RequestMethod.GET,
+//    produces = "application/json")
+//    public @ResponseBody ProductId productId(
+//            @RequestParam(value = "id") String id
+//    ){
+//        return productIdService.selectByPrimaryKey(id);
+//    }
+
+    public class Error {
+        private int code;
+        private String message;
+
+        public Error(int code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
 
 
-
-
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET)
+    public
+//    @ResponseBody
+        // 负载部分会渲染在响应体中
+    ResponseEntity<?>
+    productIdById(@PathVariable String id) {
+        ProductId productId = productIdService.selectByPrimaryKey(id);
+//        HttpStatus status = productId != null ?
+//                HttpStatus.OK : HttpStatus.NOT_FOUND;
+//        return new ResponseEntity<ProductId>(
+//                productId, status
+//        );
+        if (productId == null) {
+            Error error = new Error(4, "ProductId [" + id + "] not found");
+            return new ResponseEntity<Error>(error, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<ProductId>(productId, HttpStatus.OK);
+    }
 
 
 }
