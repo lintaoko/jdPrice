5d0e203760bdf54e014ddd2ebde1f780073e0264
xiantang <986494553@qq.com>
2019-01-14 22:12:04 +0800

    URIComponentsBuilder构建

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -5,11 +5,14 @@ import exception.UserNotFoundException;
 import model.Error;
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import service.UserService;
 
+import java.net.URI;
 import java.nio.file.ProviderNotFoundException;
 
 @RestController
@@ -22,7 +25,7 @@ public class UserController {
             method = RequestMethod.GET
     )
     public ResponseEntity<User>
-    userById(@PathVariable int id){
+    userById(@PathVariable int id) {
         User user = userService.selectById(id);
         if (user == null) {
             throw new UserNotFoundException(id);
@@ -35,14 +38,32 @@ public class UserController {
             consumes = "application/json"
     )
     @ResponseStatus(HttpStatus.CREATED)
-    public User saveUser(@RequestBody User user) {
-        // 拿到User 对象
+    public ResponseEntity<User> saveUser(@RequestBody User user,
+                                         UriComponentsBuilder ucb
+    ) {
 
-        return userService.saveUser(user);
+        // 拿到User 对象
+        User gotUser = userService.saveUser(user);
+        HttpHeaders headers = new HttpHeaders();
+//        URI locationURI = URI.create(
+//                "http://localhost:8080/jdProduct_war/user/"
+//                        + gotUser.getUserId()
+//        );
+//        headers.setLocation(locationURI);
+        URI locationURI = ucb.path("/user/").path(String.valueOf(gotUser.getUserId())).build()
+                .toUri();
+        headers.setLocation(locationURI);
+        ResponseEntity<User> responseEntity =
+                new ResponseEntity<User>(
+                        gotUser, headers, HttpStatus.CREATED
+                );
+
+        return responseEntity;
     }
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Error> userNotFound(
-          UserNotFoundException e
+            UserNotFoundException e
     ) {
         int userId = e.getUserId();
         Error error = new Error(4, "User [" + userId + "] not found");
